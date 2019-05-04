706b0a102a9a04da88aa6b21f90fcf15c62aeac2
JamesWang <jame2408@gmail.com>
2019-05-04 09:49:23 +0800

    實作驗證 Verify 功能（一個 method 完成所有功能）

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -1,6 +1,54 @@
-﻿namespace DependencyInjectionWorkshop.Models
+﻿using Dapper;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+
+namespace DependencyInjectionWorkshop.Models
 {
     public class AuthenticationService
     {
+        public bool Verify(string accountId, string password, string otp)
+        {
+            // Get DB Hash Password Using SP
+            var dbPassword = "";
+            using (var connection = new SqlConnection("my connection string"))
+            {
+                dbPassword = connection.Query<string>("spGetUserPassword", new { Id = accountId },
+                    commandType: CommandType.StoredProcedure).SingleOrDefault();
+            }
+
+            // Hash Users Key in Password
+            var crypt = new System.Security.Cryptography.SHA256Managed();
+            var hash = new StringBuilder();
+            var crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(password));
+            foreach (var theByte in crypto)
+            {
+                hash.Append(theByte.ToString("x2"));
+            }
+
+            // Get Otp From Api
+            var otpFromApi = "";
+            var httpClient = new HttpClient() { BaseAddress = new Uri("http://joey.com/") };
+            var response = httpClient.PostAsJsonAsync("api/otps", accountId).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                otpFromApi = response.Content.ReadAsAsync<string>().Result;
+            }
+            else
+            {
+                throw new Exception($"web api error, accountId:{accountId}");
+            }
+
+            // 比對 hash password & otp
+            if (dbPassword == hash.ToString() && otp == otpFromApi)
+            {
+                return true;
+            }
+
+            return false;
+        }
     }
 }
