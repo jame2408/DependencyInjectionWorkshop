5e266c64816fc6e4e73b1a516d0c4d7fba5d457a
JamesWang <jame2408@gmail.com>
2019-05-04 14:56:43 +0800

    Rename

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -7,65 +7,65 @@ namespace DependencyInjectionWorkshop.Models
 {
     public class AuthenticationService
     {
-        private readonly IProfile _profileRepo;
-        private readonly IHash _sha256Adapter;
-        private readonly IOpt _otpServiceProxy;
-        private readonly IFailedCounter _failedCounterProxy;
-        private readonly ILogger _logAdapter;
-        private readonly INotification _notifyAdapter;
+        private readonly IProfile _profile;
+        private readonly IHash _hash;
+        private readonly IOpt _otpService;
+        private readonly IFailedCounter _failedCounter;
+        private readonly ILogger _logger;
+        private readonly INotification _notification;
 
         public AuthenticationService(IProfile profileRepo, IHash sha256Adapter, IOpt otpServiceProxy, IFailedCounter failedCounterProxy, ILogger logAdapter, INotification notifyAdapter)
         {
-            _profileRepo = profileRepo;
-            _sha256Adapter = sha256Adapter;
-            _otpServiceProxy = otpServiceProxy;
-            _failedCounterProxy = failedCounterProxy;
-            _logAdapter = logAdapter;
-            _notifyAdapter = notifyAdapter;
+            _profile = profileRepo;
+            _hash = sha256Adapter;
+            _otpService = otpServiceProxy;
+            _failedCounter = failedCounterProxy;
+            _logger = logAdapter;
+            _notification = notifyAdapter;
         }
 
         public AuthenticationService()
         {
-            _profileRepo = new ProfileRepo();
-            _sha256Adapter = new Sha256Adapter();
-            _otpServiceProxy = new OtpServiceProxy();
-            _failedCounterProxy = new FailedCounterProxy();
-            _logAdapter = new LogAdapter();
-            _notifyAdapter = new NotifyAdapter();
+            _profile = new ProfileRepo();
+            _hash = new Sha256Adapter();
+            _otpService = new OtpServiceProxy();
+            _failedCounter = new FailedCounterProxy();
+            _logger = new LogAdapter();
+            _notification = new NotifyAdapter();
         }
 
         public bool Verify(string accountId, string password, string otp)
         {
             // if locked, return.
-            _profileRepo.CheckAccountIsLocked(accountId);
+            _profile.CheckAccountIsLocked(accountId);
 
             // Get DB Hash Password Using SP
-            var passwordFromDb = _profileRepo.GetProfile(accountId);
+            var passwordFromDb = _profile.GetProfile(accountId);
 
             // Hash Users Key in Password
-            var hashedPassword = _sha256Adapter.GetHashed(password);
+            var hashedPassword = _hash.GetHashed(password);
 
             // Get Otp From Api
-            var currentOpt = _otpServiceProxy.GetCurrentOpt(accountId);
+            var currentOpt = _otpService.GetCurrentOpt(accountId);
 
             // 比對 hash password & otp
             if (passwordFromDb == hashedPassword.ToString() && otp == currentOpt)
             {
                 // Verify success reset count
-                _failedCounterProxy.Reset(accountId);
+                _failedCounter.Reset(accountId);
 
                 return true;
             }
 
             // Verify failed count add 1
-            _failedCounterProxy.Add(accountId);
+            _failedCounter.Add(accountId);
 
             // 取得失敗次數，並用 NLog 記錄 log 資訊。
-            var failedCount = _failedCounterProxy.Get(accountId);
-            _logAdapter.Info(accountId, failedCount);
+            var failedCount = _failedCounter.Get(accountId);
+            _logger.Info(accountId, failedCount);
 
             // 比對失敗用 Slack 通知使用者
-            _notifyAdapter.PushNotify(accountId, $"accountId:{accountId} verify failed.");
+            _notification.PushNotify(accountId, $"accountId:{accountId} verify failed.");
 
             return false;
         }
